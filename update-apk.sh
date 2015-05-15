6c6683977f8c27d74cdf16a7b5f081dd11760986
Xudong Ma <simonma@google.com>
2015-05-14 19:01:18 -0700

    Don't build release apk in script update-apk.sh

--- update-apk.sh
+++ update-apk.sh
@@ -12,8 +12,8 @@ cd ../.. && rm -rf grpc-java
 
 echo "[INFO] Building Apk"
 git pull origin master
-./gradlew assemble
-./gradlew assembleAndroidTest
+./gradlew assembleDebug
+./gradlew assembleDebugAndroidTest
 
 echo "[INFO] Installing Apk"
 adb -e install -r app/build/outputs/apk/app-debug-unaligned.apk
